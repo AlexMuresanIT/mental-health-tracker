79ba80f00f1c4b8576186da881e704c30bc69a03
Alexandru Muresan <102810131+AlexMuresanIT@users.noreply.github.com>
2025-10-18 20:44:32 +0300

    add workflows (#3)
    * add workflows
    
    * chore: spotless
    
    * chore: spotless

--- src/main/java/com/health/mental/MentalApplication.java
+++ src/main/java/com/health/mental/MentalApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MentalApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MentalApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(MentalApplication.class, args);
+  }
 }
